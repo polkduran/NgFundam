58c3126de1681e37bb73b616240ae6c422eef056
polkduran <polkduran@hotmail.com>
2018-07-17 15:03:02 +0200

    Add images

--- src/app/events-app.component.ts
+++ src/app/events-app.component.ts
@@ -2,7 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'events-app-root',
-  template: `<h2>yep</h2>`
+  template: `
+  <h2>yep</h2>
+  <img src="assets/images/basic-shield.png"/>
+  `
 })
 export class EventsAppComponent {
   title = 'app';
